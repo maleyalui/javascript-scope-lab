b0b92c53572bd4bf5fb17431afbac7a22f910910
maleyalui <maleyalui1@gmail.com>
2025-12-06 11:54:47 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+
+// Write your solution in this file! 
+
+//Global array
+const burgers = ["Hamburger","Cheeseburger"];
+ //Global drink
+
+  let featuredDrink = "Strawberry Milkshake"; 
+
+//Function that adds a new burger 
+
+  function addBurger(){
+     //function-scoped variable 
+    const newBurger = "Flatburger";
+    
+     //Add to the burgers array 
+     burgers.push(newBurger);
+     
+if(true) {const anotherNewBurger = 'Maple Bacon Burger'; burgers.push(anotherNewBurger)}; } 
+
+function changeFeaturedDrink(){ 
+    featuredDrink = "The JavaShake"; }
